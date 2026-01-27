dafefa558d397ec4600ff6693471f5261b404852
Anisha Patel <anisha63911@gmail.com>
2026-01-27 09:24:52 +0530

    commit

---
+++ filesystem.js
@@ -0,0 +1,48 @@
+let fs = require("fs").promises;
+let {promisify} = require("util");  //filesystemmodule
+let Anisha = promisify(require("fs").readFile);
+
+
+fs.writeFile("cb.txt",
+    "this is simple call back file data",(error)=>{
+        if(error){
+            console.log("file is loading error", error);
+        } else{
+            console.log("File is created with simple call back");
+        }
+    });
+// fs.readFile(()=>{
+
+// })
+async function readFileWithPromises(){
+    try{
+        let data = 
+        await fs.readFile("cb.txt","utf-8");
+
+        console.log(
+            "Async file data with promises:", data);
+
+    }catch(error){
+        console.log(
+            "Error is loading async file read error:", error);
+    }
+}
+readFileWithPromises();
+
+async function readFileWithPromisify() {
+    try {
+        let  fileData =
+        await Anisha("cb.txt", "utf-8");
+        console.log(
+            "Reading the file data with custom(promisyfy)", fileData);
+
+    }catch (error) {
+        console.log(error)
+
+    }
+
+    
+}
+readFileWithPromisify();
+
+
