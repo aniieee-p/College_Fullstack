17c89d99f22e814de06397721d48942fae4ec0d8
Anisha Patel <anisha63911@gmail.com>
2026-02-04 10:11:08 +0530

    Added .gitignore and updated project files

--- index1.js
+++ index1.js
@@ -7,12 +7,27 @@ const port = 8000;
 app.set('view engine', 'ejs');
 app.set('views' , path.join(__dirname, 'views'));
 
+//what is path .join?
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.render("index", {title: "Home Page", name: "aniieee"});
+    res.render("index", {title: "Home Page", name: "aniieee", email:"contact@gmail.com"});
 });
+
+app.get("/about",(req,res)=>{
+    res.render("about",{
+        title:"About_us"
+    });
+})
+
+app.get("/contact",(req,res)=>{
+    res.render("contact"),{
+        title:"Contact-us"
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
